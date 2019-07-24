ca427f96e5543cc7b422c21e4a4d4fb73323430d
karan1414 <36893447+karan1414@users.noreply.github.com>
2019-07-24 16:09:50 +0530

    Update app.py

--- SfaB_Test/app.py
+++ SfaB_Test/app.py
@@ -4,34 +4,35 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-SECRET_KEY = os.urandom (256)
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+# SECRET_KEY = os.urandom (256)
+# SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-# GCP
-CLOUDSQL_USER = os.environ.get("DB_USER")
-CLOUDSQL_PASSWORD = os.environ.get("DB_PASS")
-CLOUDSQL_DATABASE = os.environ.get("DB_NAME")
-CLOUDSQL_CONNECTION_NAME = os.environ.get("CLOUD_SQL_CONNECTION_NAME")
-LOCAL_SQLALCHEMY_DATABASE_URI = (
-    'mysql+pymysql://{nam}:{pas}@127.0.0.1:3306/{dbn}').format (
-    nam=CLOUDSQL_USER,
-    pas=CLOUDSQL_PASSWORD,
-    dbn=CLOUDSQL_DATABASE,
-)
+# # GCP
+# CLOUDSQL_USER = os.environ.get("DB_USER")
+# CLOUDSQL_PASSWORD = os.environ.get("DB_PASS")
+# CLOUDSQL_DATABASE = os.environ.get("DB_NAME")
+# CLOUDSQL_CONNECTION_NAME = os.environ.get("CLOUD_SQL_CONNECTION_NAME")
+# LOCAL_SQLALCHEMY_DATABASE_URI = (
+#     'mysql+pymysql://{nam}:{pas}@127.0.0.1:3306/{dbn}').format (
+#     nam=CLOUDSQL_USER,
+#     pas=CLOUDSQL_PASSWORD,
+#     dbn=CLOUDSQL_DATABASE,
+# )
 
-LIVE_SQLALCHEMY_DATABASE_URI = (
-    'mysql+pymysql://{nam}:{pas}@localhost/{dbn}?unix_socket=/cloudsql/{con}').format (
-    nam=CLOUDSQL_USER,
-    pas=CLOUDSQL_PASSWORD,
-    dbn=CLOUDSQL_DATABASE,
-    con=CLOUDSQL_CONNECTION_NAME,
-)
+# LIVE_SQLALCHEMY_DATABASE_URI = (
+#     'mysql+pymysql://{nam}:{pas}@localhost/{dbn}?unix_socket=/cloudsql/{con}').format (
+#     nam=CLOUDSQL_USER,
+#     pas=CLOUDSQL_PASSWORD,
+#     dbn=CLOUDSQL_DATABASE,
+#     con=CLOUDSQL_CONNECTION_NAME,
+# )
 
-if os.environ.get ('GAE_INSTANCE'):
-    SQLALCHEMY_DATABASE_URI = LIVE_SQLALCHEMY_DATABASE_URI
-else:
-    SQLALCHEMY_DATABASE_URI = LOCAL_SQLALCHEMY_DATABASE_URI
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+# if os.environ.get ('GAE_INSTANCE'):
+#     SQLALCHEMY_DATABASE_URI = LIVE_SQLALCHEMY_DATABASE_URI
+# else:
+#     SQLALCHEMY_DATABASE_URI = LOCAL_SQLALCHEMY_DATABASE_URI
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -49,8 +50,23 @@ class Location(db.Model):
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Flippingo !!'
+#     return 'Flippingo !!'
+       loc = Location.query.all()
 
+   data = []
+
+   print(loc)
+   for l in loc:
+       data.append({
+        "latitude" : l.latitude,
+        "longitude" : l.longitude,
+        "angle"     : l.angle,
+        "direction" : l.direction
+        })
+    # print(l.latitude)
+
+   return jsonify({"data":data})
+    
 # saves the location data
 
 @app.route('/location_save',methods=['POST'])
@@ -116,5 +132,5 @@ def get_all():
 
 if __name__ == "__main__":
     db.create_all()
-    # app.run(host="0.0.0.0", port=80)
-    app.run(host="127.0.0.1", port=5000,debug=True)
+    app.run(host="0.0.0.0", port=8080)
+#     app.run(host="127.0.0.1", port=5000,debug=True)
